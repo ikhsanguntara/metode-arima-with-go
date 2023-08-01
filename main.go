26a1bb4317994ef60edf7f669460e1145ecd26ab
ikhsan <ikhsan.guntara@magnatech.co.id>
2023-08-01 23:12:17 +0700

    update mse di smooting

--- main.go
+++ main.go
@@ -18,20 +18,25 @@ func exponentialSmoothing(data []float64, alpha float64) []float64 {
 	return smoothedData
 }
 
-func calculateMAE(actualData, predictedData []float64) float64 {
+func calculateMAEAndMSE(actualData, predictedData []float64) (float64, float64) {
 	if len(actualData) != len(predictedData) {
 		panic("Panjang data aktual dan data prediksi harus sama")
 	}
 
-	totalAbsoluteError := 0.0
 	n := float64(len(actualData))
+	totalAbsoluteError := 0.0
+	totalSquaredError := 0.0
 
 	for i := 0; i < len(actualData); i++ {
-		totalAbsoluteError += math.Abs(actualData[i] - predictedData[i])
+		absoluteError := math.Abs(actualData[i] - predictedData[i])
+		totalAbsoluteError += absoluteError
+		squaredError := math.Pow(absoluteError, 2)
+		totalSquaredError += squaredError
 	}
 
 	mae := totalAbsoluteError / n
-	return mae
+	mse := totalSquaredError / n
+	return mae, mse
 }
 
 func main() {
@@ -48,7 +53,65 @@ func main() {
 	// GROUP BY start_of_week, end_of_week
 	// ORDER BY start_of_week ASC;
 
-	data := []float64{66, 113, 116, 135, 90, 129, 79, 118, 98, 111, 153, 123, 140, 144, 140, 145, 73, 84, 131, 137, 136, 101, 135, 106, 102, 128, 138, 104, 116, 94, 118, 126, 144, 138, 109, 136, 134, 121, 129, 147, 113, 102, 104, 70, 138, 78, 86, 96, 136, 89, 124, 131, 93, 108, 151, 116, 111}
+	data := []float64{
+		4,
+		122,
+		113,
+		116,
+		135,
+		90,
+		129,
+		79,
+		118,
+		98,
+		111,
+		153,
+		123,
+		140,
+		144,
+		140,
+		145,
+		73,
+		84,
+		131,
+		137,
+		136,
+		101,
+		135,
+		106,
+		102,
+		128,
+		138,
+		104,
+		116,
+		94,
+		118,
+		126,
+		144,
+		138,
+		109,
+		136,
+		134,
+		121,
+		129,
+		147,
+		113,
+		102,
+		104,
+		70,
+		138,
+		78,
+		86,
+		96,
+		136,
+		89,
+		124,
+		131,
+		93,
+		108,
+		151,
+		116,
+		111}
 
 	alpha := 0.3
 
@@ -77,7 +140,8 @@ func main() {
 	fmt.Println("\nPrediksi 12 Minggu Ke Depan:")
 	fmt.Println(predictionData)
 
-	// Menghitung MAE untuk hasil peramalan
-	mae := calculateMAE(data, smoothedData)
+	// Menghitung MAE dan MSE untuk hasil peramalan
+	mae, mse := calculateMAEAndMSE(data, smoothedData)
 	fmt.Println("\nMean Absolute Error (MAE) untuk hasil peramalan:", mae)
+	fmt.Println("Mean Squared Error (MSE) untuk hasil peramalan:", mse)
 }
